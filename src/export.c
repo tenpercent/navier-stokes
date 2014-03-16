8faf9a4d6fb69485171c12781daa6eb8717e9acb
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-03-17 02:29:01 +0400

    cross-platform intro

--- src/export.c
+++ src/export.c
@@ -4,19 +4,25 @@
 
 #define MAX_BUFFER_SIZE 10000
 
+#ifdef _WIN32
+#define ENDLINE \n\r
+#else
+#define ENDLINE \n
+#endif
+
 void export_results (double ** results, unsigned total_experiments) {
   FILE * csv_data; 
   unsigned experiments_step = 0;
   // dangerous shit begins
   char * result_to_string = 
-    ",Time elapsed,V residual in C,V residual in L2,G residual in C,G residual in L2\n\r";
+    ",Time elapsed,V residual in C,V residual in L2,G residual in C,G residual in L2 ENDLINE";
   char * current_experiment_to_string = "";
   char * combined = "";
   for (experiments_step = 0; experiments_step < total_experiments; ++experiments_step) {
     snprintf (
       current_experiment_to_string,
       MAX_BUFFER_SIZE,
-      ",%.3lf s,%.6lf,%.6lf,%.6lf,%.6lf\n\r", 
+      ",%.3lf s,%.6lf,%.6lf,%.6lf,%.6lf ENDLINE", 
         results[0][experiments_step], 
         results[1][experiments_step], 
         results[2][experiments_step], 
