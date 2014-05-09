8673b3b1f3b1de1eef0bb97b8b641d3b95c6b571
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-05-09 18:01:51 +0400

    separate logic of matrix filling

--- include/pdesolver/calculation.h
+++ include/pdesolver/calculation.h
@@ -20,20 +20,6 @@ void find_approximate_solution (
   Grid const * grid,
   Iterative_Method_parameters const * iterative_method_parameters);
 
-// fill the system of linear equations
-// which purpose is to find the values of sought functions
-// in the mesh nodes of next time layer
-void fill_system (
-    Sparse_matrix * lh_side,
-    double * rh_side,
-    Grid const * grid,
-    Node_status const * node_status,
-    Gas_parameters const * parameters,
-    double const * space_coordinates,
-    unsigned const time_step,
-    double const * G,
-    double const * V);
-
 // fill the arrays (V[]) and (G[])
 // with the values of sought functions
 // in the nodes of the initial time layer.
