0abb1c350d265013ec5197feee8160e95aa4f569
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-05-10 02:18:15 +0400

    beautify

--- src/graphics/plot_data.py
+++ src/graphics/plot_data.py
@@ -1,14 +1,17 @@
 from numpy import loadtxt
 from glob import glob
 from pylab import plot, savefig, clf
+from os.path import splitext, basename
 
 def main():
   results_path = "../../build/results/"
   files = glob(results_path + "*.dat")
   tables = [loadtxt(f, skiprows=1) for f in files]
+  basenames = [splitext(basename(f))[0] for f in files]
+
   for (index, table) in enumerate(tables):
     plot(table[:,0], table[:,1])
-    savefig('%d.png' % index)
+    savefig('%s.png' % basenames[index])
     clf()
   return
 
