a7d301415d6fc8d309fd987433206a5caefafafe
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-05-10 02:22:41 +0400

    beautify

--- src/graphics/plot_data.py
+++ src/graphics/plot_data.py
@@ -1,17 +1,22 @@
 from numpy import loadtxt
 from glob import glob
 from pylab import plot, savefig, clf
-from os.path import splitext, basename
+from os.path import splitext, basename, exists
+from os import makedirs
 
 def main():
   results_path = "../../build/results/"
+  ans_directory_name = "png"
   files = glob(results_path + "*.dat")
   tables = [loadtxt(f, skiprows=1) for f in files]
   basenames = [splitext(basename(f))[0] for f in files]
 
+  if not exists(ans_directory_name):
+    makedirs(ans_directory_name)
+
   for (index, table) in enumerate(tables):
     plot(table[:,0], table[:,1])
-    savefig('%s.png' % basenames[index])
+    savefig('%s/%s.png' % (ans_directory_name, basenames[index]))
     clf()
   return
 
