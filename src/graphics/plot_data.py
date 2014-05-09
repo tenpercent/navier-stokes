1b7d94d787bbcc5bd935bfd19f22aedb2cead286
Max Podkorytov <max.podkorytoff@yandex.ru>
2014-05-10 02:10:25 +0400

    clearify

--- src/graphics/plot_data.py
+++ src/graphics/plot_data.py
@@ -1,6 +1,6 @@
 from numpy import loadtxt
 from glob import glob
-from pylab import plot, savefig
+from pylab import plot, savefig, clf
 
 def main():
   results_path = "../../build/results/"
@@ -9,6 +9,7 @@ def main():
   for (index, table) in enumerate(tables):
     plot(table[:,0], table[:,1])
     savefig('%d.png' % index)
+    clf()
   return
 
 main()
