a9606dc392523b286be989fe8d7d31a0b5290efc
Dmitry Shachnev <mitya57@gmail.com>
2014-05-11 10:29:37 +0400

    Fix building with standard C 98/11

--- src/pdesolver/start_conditions_12.c
+++ src/pdesolver/start_conditions_12.c
@@ -1,6 +1,10 @@
 #include <math.h>
 #include "start_conditions.h"
 
+#ifndef M_PI
+  #define M_PI 3.14159265358979323846264338327950288
+#endif
+
 double u_start (double x) {
   return sin(M_PI * x * x * .01);
 }
