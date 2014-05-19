4b2260407bd43f29a3487c395186ea0d980895e8
Dmitry Shachnev <mitya57@gmail.com>
2014-05-19 18:18:37 +0400

    Style and code fixes to plot_data.py

--- src/graphics/plot_data.py
+++ src/graphics/plot_data.py
@@ -1,30 +1,25 @@
+#!/usr/bin/env python3
+
 from numpy import loadtxt
 from glob import glob
 from pylab import plot, grid, xlabel, ylabel, title, savefig, clf
+from os.path import basename, exists, join, splitext
 
 import os
 import argparse
-import sys
 
 
 def make_argument_parser():
-
     parser = argparse.ArgumentParser(description="Plot graphics using .dat files in directory")
-    parser.add_argument('-i', help="input directory")
-
-    if not len(sys.argv) > 1:
-        msg = "empty argument list"
-        parser.print_help()
-        raise argparse.ArgumentTypeError(msg)
-
+    parser.add_argument('input_directory', help="input directory")
     return parser
 
 
 def parse_num(string):
-    return int (string[5])
+    return int(string[5])
 
 
-def plot_figure (index, table, basenames, ans_directory_name):
+def plot_figure(index, table, basenames, ans_directory_name):
     clf()
     xlabel("x")
     ylabel(basenames[index][0], dict(rotation=0,
@@ -34,30 +29,30 @@ def plot_figure (index, table, basenames, ans_directory_name):
     title("%s(x, t) at t = %d" % (basenames[index][0], parse_num(basenames[index]) + 1))
     plot(table[:,0], table[:,1])
     grid(True)
-    savefig('%s/%s.png' % (ans_directory_name, basenames[index]))
-    return
+    filename = join(ans_directory_name, '%s.png' % basenames[index])
+    savefig(filename)
+    print('Saved %s' % filename)
 
 
 def main():
     args = make_argument_parser().parse_args()
-    results_dir = args.i
+    results_dir = args.input_directory
 
-    results_subdirs = os.walk(results_dir).next()[1]
+    results_subdirs = next(os.walk(results_dir))[1]
 
     for subdir in results_subdirs:
+        files = glob(join(results_dir, subdir, '*.dat'))
+        print('Files in %s: %s' % (subdir, ', '.join(files) or 'no files'))
+        ans_directory_name = join(results_dir, subdir, 'png')
 
-        files = glob(results_dir + subdir + '/' + "*.dat")
-        ans_directory_name = results_dir + subdir + '/png'
+        basenames = [splitext(basename(f))[0] for f in files]
 
-        tables = [loadtxt(f) for f in files]
-        basenames = [os.path.splitext(os.path.basename(f))[0] for f in files]
-
-        if not os.path.exists(ans_directory_name):
+        if not exists(ans_directory_name):
             os.makedirs(ans_directory_name)
 
-        for (index, table) in enumerate(tables):
-            plot_figure(index, table, basenames, ans_directory_name)
-    return
+        for index, f in enumerate(files):
+            plot_figure(index, loadtxt(f), basenames, ans_directory_name)
 
 
-main()
+if __name__ == '__main__':
+    main()
